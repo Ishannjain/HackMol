5152a5384d12947e97794cd85a35b323e1122e10
DevanshArdhiya <dev.ardhiya.01@gmail.com>
2025-04-11 00:45:03 +0530

    CreateAuctionFrontEnd

--- auction/views.py
+++ auction/views.py
@@ -115,7 +115,7 @@ def create_listing(request):
 
 
         cat_id=request.POST.get("category")
-        category = Category.objects.filter(pk = cat_id).first()
+        category = Category.objects.filter(title = cat_id).first()
 
         if mode == "private" :
             listing=Listing(
